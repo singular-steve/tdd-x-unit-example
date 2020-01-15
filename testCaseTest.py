44913b38ea84aad022312de3113f8691e3451660
singular-steve <singular1.steve@gmail.com>
2020-01-15 16:51:22 +0900

    - add tearDown function - add log when setUp and tearDown

--- testCaseTest.py
+++ testCaseTest.py
@@ -6,14 +6,10 @@ class TestCaseTest(TestCase):
     def setUp(self):
         self.test = WasRun("testMethod")
 
-    def testRunning(self):
-        self.test.run()
-        assert (self.test.wasRun)
-
-    def testSetUp(self):
-        self.test.run()
-        assert (self.test.wasSetUp)
+    def testTemplateMethod(self):
+        test = WasRun("testMethod")
+        test.run()
+        assert ("setUp testMethod tearDown " == test.log)
 
 
-TestCaseTest("testRunning").run()
-TestCaseTest("testSetUp").run()
+TestCaseTest("testTemplateMethod").run()
